d6fac7e818decac100c8d76db1b54dae2c08ba29
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-05-15 14:21:31 +0500

    исправил Device Orientation

--- imageFeed/ImagesList/ImagesListCell.swift
+++ imageFeed/ImagesList/ImagesListCell.swift
@@ -9,5 +9,9 @@ import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
 }
