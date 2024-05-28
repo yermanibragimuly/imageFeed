eb419faded536f032d8a5a0c01e07edddb396570
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-05-28 14:46:29 +0500

    Переход с таблицы к SingleImageView

---
+++ imageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  imageFeed
+//
+//  Created by Yerman Ibragimuly on 28.05.2024.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
