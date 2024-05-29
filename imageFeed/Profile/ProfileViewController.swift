7d0e6d4f68be41545563f8ac4761216fa2dff435
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-05-29 22:13:22 +0500

    верстка кодом profileView

--- imageFeed/Profile/ProfileViewController.swift
+++ imageFeed/Profile/ProfileViewController.swift
@@ -8,13 +8,69 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet var avatarImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var loginNameLable: UILabel!
-    @IBOutlet var descriptionLabel: UILabel!
+    private var label: UILabel?
     
-    @IBOutlet var logoutButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let profileImage = UIImage(named: "avatar")
+        let imageView = UIImageView(image: profileImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.font = UIFont.boldSystemFont(ofSize: 23)
+        label.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        self.label = label
+        
+        let label2 = UILabel()
+        label2.text = "@ekaterina_nov"
+        label2.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0)
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label2)
+        label2.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true
+        label2.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8).isActive = true
+        self.label = label2
+        
+        let label3 = UILabel()
+        label3.text = "Hello, world!"
+        label3.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label3)
+        label3.leadingAnchor.constraint(equalTo: label2.leadingAnchor).isActive = true
+        label3.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 8).isActive = true
+        self.label = label3
+        
+        let button = UIButton.systemButton(
+            with: UIImage(named: "logout_button")!,
+            target: self,
+            action: #selector(didTapButton)
+        )
+        button.tintColor = UIColor(red: 245/255, green: 107/255, blue: 108/255, alpha: 1.0)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+    }
     
-    @IBAction private func didTapLogoutButton() {
+    @objc func didTapButton() {
+        for subview in view.subviews {
+            if subview is UILabel {
+                subview.removeFromSuperview()
+            }
+        }
     }
 }
+
--- imageFeed/SingleImageViewController/SingleImageViewController.swift
+++ imageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -11,27 +11,27 @@ class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
             guard isViewLoaded, let image else { return }
-
+            
             imageView.image = image
             imageView.frame.size = image.size
             rescaleAndCenterImageInScrollView(image: image)
         }
     }
-
+    
     @IBOutlet private var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
-
+        
         guard let image else { return }
         imageView.image = image
         imageView.frame.size = image.size
         rescaleAndCenterImageInScrollView(image: image)
     }
-
+    
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
