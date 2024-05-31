873b9302d580edf14835748bc57f0e82330c50f7
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-05-31 11:40:55 +0500

    update code in ProfileViewController

--- imageFeed/Profile/ProfileViewController.swift
+++ imageFeed/Profile/ProfileViewController.swift
@@ -17,38 +17,46 @@ final class ProfileViewController: UIViewController {
         let imageView = UIImageView(image: profileImage)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imageView)
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
         imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
         imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
         
-        let label = UILabel()
-        label.text = "Екатерина Новикова"
-        label.font = UIFont.boldSystemFont(ofSize: 23)
-        label.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-        label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
-        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
-        self.label = label
+        let descriptionLabelName = UILabel()
+        descriptionLabelName.text = "Екатерина Новикова"
+        descriptionLabelName.numberOfLines = 0
+        descriptionLabelName.font = UIFont.boldSystemFont(ofSize: 23)
+        descriptionLabelName.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        descriptionLabelName.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabelName)
+        descriptionLabelName.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        descriptionLabelName.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        descriptionLabelName.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        self.label = descriptionLabelName
         
-        let label2 = UILabel()
-        label2.text = "@ekaterina_nov"
-        label2.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0)
-        label2.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label2)
-        label2.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true
-        label2.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8).isActive = true
-        self.label = label2
+        let descriptionLabelLogin = UILabel()
+        descriptionLabelLogin.text = "@ekaterina_nov"
+        descriptionLabelLogin.numberOfLines = 0
+        descriptionLabelLogin.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0)
+        descriptionLabelLogin.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabelLogin)
+        descriptionLabelLogin.trailingAnchor.constraint(equalTo: descriptionLabelName.trailingAnchor).isActive = true
+        descriptionLabelLogin.leadingAnchor.constraint(equalTo: descriptionLabelName.leadingAnchor).isActive = true
+        descriptionLabelLogin.topAnchor.constraint(equalTo: descriptionLabelName.bottomAnchor, constant: 8).isActive = true
+        self.label = descriptionLabelLogin
         
-        let label3 = UILabel()
-        label3.text = "Hello, world!"
-        label3.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
-        label3.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label3)
-        label3.leadingAnchor.constraint(equalTo: label2.leadingAnchor).isActive = true
-        label3.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 8).isActive = true
-        self.label = label3
+        let descriptionLabelStatus = UILabel()
+        descriptionLabelStatus.text = "Hello, world!"
+        descriptionLabelStatus.numberOfLines = 0
+        descriptionLabelStatus.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        descriptionLabelStatus.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabelStatus)
+        descriptionLabelStatus.trailingAnchor.constraint(equalTo: descriptionLabelLogin.trailingAnchor).isActive = true
+        descriptionLabelStatus.leadingAnchor.constraint(equalTo: descriptionLabelLogin.leadingAnchor).isActive = true
+        descriptionLabelStatus.topAnchor.constraint(equalTo: descriptionLabelLogin.bottomAnchor, constant: 8).isActive = true
+        self.label = descriptionLabelStatus
         
         let button = UIButton.systemButton(
             with: UIImage(named: "logout_button")!,
