957955ab45e25e7409b92510f682f924889fc7d1
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-06-27 17:13:09 +0500

    fix bag

--- imageFeed/Constants.swift
+++ imageFeed/Constants.swift
@@ -12,7 +12,7 @@ enum Constants {
     static let secretKey = "pSvOi0gGSFm0yWvOn9tLEtKr6JOEHSmJDDwMHiPoD3g"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL = URL(string: "https://unsplash.com/")!
     static let authPath: String = "oauth/token/"
     static let apiURL = URL(string: "https://api.unsplash.com/")
 }
