561b9808406eecfb6e849ec5aa408efe9067f5a3
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-09-20 11:47:58 +0500

    Испраил код, по рекомендации ревьювера

---
+++ imageFeed/OAuth2 API/OAuth2Service.swift
@@ -0,0 +1,61 @@
+
+import Foundation
+
+final class OAuth2Service {
+    // MARK: - Private Properties
+    static let shared = OAuth2Service()
+    private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
+    private (set) var authToken: String? {
+        get { return OAuth2TokenStorage().token }
+        set { OAuth2TokenStorage().token = newValue }
+    }
+    
+    // MARK: - Public Methods
+    func fetchOAuthToken(
+        _ code: String,
+        completion: @escaping (Result<String, Error>) -> Void
+    ) {
+        assert(Thread.isMainThread)
+        if lastCode == code { return }
+        task?.cancel()
+        lastCode = code
+        
+        guard let request = authTokenRequest(code: code) else { return }
+        let task = urlSession.objectTask(for: request) { [weak self] (result:
+            Result<OAuthTokenResponseBody, Error>) in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let body):
+                    let authToken = body.accessToken
+                    self.authToken = authToken
+                    completion(.success(authToken))
+                    self.task = nil
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+        self.task = task
+        task.resume()
+    }
+}
+
+private extension OAuth2Service {
+    
+    func authTokenRequest(code: String) -> URLRequest? {
+        URLRequest.makeHTTPRequest(
+            path: "/oauth/token"
+            + "?client_id=\(KeyAndUrl.accessKey)"
+            + "&&client_secret=\(KeyAndUrl.secretKey)"
+            + "&&redirect_uri=\(KeyAndUrl.redirectUrl)"
+            + "&&code=\(code)"
+            + "&&grant_type=authorization_code",
+            httpMethod: "POST",
+            baseURL: URL(string: "https://unsplash.com")!
+        )
+    }
+}
--- imageFeed/Profile/ProfileService.swift
+++ imageFeed/Profile/ProfileService.swift
@@ -1,114 +1,53 @@
-import UIKit
+
+import Foundation
 
 final class ProfileService {
     
+    //MARK: - Private Properties
     static let shared = ProfileService()
-    
     private var task: URLSessionTask?
+    private let urlSession = URLSession.shared
     private(set) var profile: Profile?
-    
     private init() {}
     
-    private func createProfileURLRequest(token: String) -> URLRequest? {
-        guard let profileURL = URL(string: ProfileConstants.urlProfilePath) else {
-            print(NetworkError.invalidURL)
-            return nil
-        }
-        var request = URLRequest(url: profileURL)
-        request.httpMethod = "GET"
-        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        return request
-    }
-    
-    func fetchProfile(
-        _ token: String,
-        completion: @escaping (Result<(Profile, String), Error>) -> Void
-    ) {
+    // MARK: - Public Methods
+    func fetchProfile(_ token: String, completion: @escaping (Result<Profile, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        if profile != nil { return }
         task?.cancel()
-        guard let request = createProfileURLRequest(token: token) else {
-            completion(.failure(NetworkError.invalidRequest))
-            return
-        }
-        task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            defer {
-                DispatchQueue.main.async {
-                    UIBlockingProgressHUD.dismiss()
-                }
-            }
-            guard let self = self else { return }
-            if let error = error {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-                return
-            }
-            guard let httpResponse = response as? HTTPURLResponse else {
-                DispatchQueue.main.async {
-                    completion(.failure(NetworkError.unknownError))
-                }
-                return
-            }
-            switch httpResponse.statusCode {
-            case 200:
-                guard let data = data else {
-                    DispatchQueue.main.async {
-                        completion(.failure(NetworkError.emptyData))
-                    }
-                    return
-                }
-                do {
-                    let profileResult = try JSONDecoder().decode(ProfileResult.self, from: data)
-                    let profile = Profile(result: profileResult)
+        let request = profileRequest(token: token)
+        let task = urlSession.objectTask(for: request) { [weak self] (result: Result<ProfileResult, Error>) in
+                guard let self = self else { return }
+                switch result {
+                case .success(let body):
+                    let profile = Profile(callData: body)
                     self.profile = profile
-                    DispatchQueue.main.async {
-                        completion(.success((profile, profileResult.username)))
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        completion(.failure(error))
-                    }
-                }
-            default:
-                let error = NetworkErrorHandler.handleErrorResponse(statusCode: httpResponse.statusCode)
-                DispatchQueue.main.async {
+                    completion(.success(profile))
+                    self.task = nil
+                case .failure(let error):
                     completion(.failure(error))
                 }
             }
+        self.task = task
+        task.resume()
+    }
+    
+    func clean() {
+            profile = nil
         }
-        task?.resume()
-    }
 }
 
-struct ProfileResult: Codable {
-    let username: String
-    let firstName: String?
-    let lastName: String?
-    let bio: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case username
-        case bio
-    }
-}
-
-struct Profile: Decodable {
-    let username: String
-    let name: String?
-    let loginName: String
-    let bio: String?
-    
-    init(result: ProfileResult) {
-        self.username = result.username
-        self.name = "\(result.firstName ?? "") \(result.lastName ?? "")"
-        self.loginName = "@\(result.username)"
-        self.bio = result.bio
-    }
-}
-
-extension ProfileService {
-    func clearProfile() {
-        profile = nil
+// MARK: - Extension
+private extension ProfileService {
+    func profileRequest(token: String) -> URLRequest {
+        guard let url = URL(
+            string: "\(KeyAndUrl.defaultBaseApiUrl)"
+            + "/me")
+        else {
+            fatalError("Failed to create URL")
+        }
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        return request
     }
 }
