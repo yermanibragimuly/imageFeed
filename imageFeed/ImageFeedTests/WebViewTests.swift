4118a48f6b3fc7eef7ade04027916bf2be46dbf6
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-09-24 13:54:55 +0500

    тест AuthHelper-а

--- imageFeed/ImageFeedTests/WebViewTests.swift
+++ imageFeed/ImageFeedTests/WebViewTests.swift
@@ -64,4 +64,21 @@ final class WebViewTests: XCTestCase {
         //then
         XCTAssertTrue(shouldHideProgress)
     }
+    
+    func testAuthHelperAuthURL() {
+        //given
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        //when
+        let url = authHelper.authURL()
+        let urlString = url.absoluteString
+        
+        //then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+    }
 }
