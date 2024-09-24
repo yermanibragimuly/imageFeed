f34f9b680e3d65fe98b3f339f3022608c0fde3b3
yermanibragimuly <yerman.ibragimuly@gmail.com>
2024-09-24 13:51:08 +0500

    выполнена самостоятельная задача 2

--- imageFeed/ImageFeedTests/WebViewTests.swift
+++ imageFeed/ImageFeedTests/WebViewTests.swift
@@ -38,4 +38,30 @@ final class WebViewTests: XCTestCase {
         //then
         XCTAssertTrue(viewController.loadRequestCalled)
     }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressHiddenWhenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertTrue(shouldHideProgress)
+    }
 }
